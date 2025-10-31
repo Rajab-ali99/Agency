5e0eb72579b4dd2af5201ebfbdef79e4b3300682
Rajab-ali99 <rajabli2649gmail.com>
2025-10-31 17:02:25 +0500

    responsiveness

--- app/page.jsx
+++ app/page.jsx
@@ -19,21 +19,21 @@ export default function Home() {
     <div >
       <HeroSection />
       {/* HeroSection image */}
-      <div className="flex overflow-hidden relative justify-center ">
+      <div className="flex md:overflow-hidden relative justify-center ">
         <div className="absolute inset-0 bg-gradient-to-t  from-emerald-500/10 via-emerald-500/60 to-transparent blur-3xl" />
         <div className="absolute z-20 inset-0 bg-gradient-to-t bottom-0  from-black/90 via-black/10 to-transparent" />
 
-        <div className="absolute flex w-[100vw] gap-25 items-center   mx-auto top-[300px] text-9xl font-black whitespace-nowrap animate-marquee">
+        <div className="absolute  flex w-[100vw] gap-25 items-center top-[130px]  mx-auto md:top-[300px] text-4xl md:text-9xl font-black whitespace-nowrap animate-marqueee">
 
 
           <div className="w-full ps-25">Best Markiting Agency</div>
           <div className="w-full ">Best Markiting Agency</div>
         </div>
 
-        <Image className="w-[80vw] z-10" quality={100} src={HeroImg.src} height={HeroImg.height} width={HeroImg.width} alt="Hero Image" />
+        <Image className="md:w-[80vw] z-10" quality={100} src={HeroImg.src} height={HeroImg.height} width={HeroImg.width} alt="Hero Image" />
       </div>
 
-      <div className=" relative left-[170px] -mt-[110px] bg-transparent z-50  rounded-4xl w-sm py-4 px-5 border border-slate-600">
+      <div className=" hidden md:block relative left-[170px] -mt-[110px] bg-transparent z-50  rounded-4xl w-sm py-4 px-5 border border-slate-600">
         <div className="text-4xl pb-2   border-gray-500"><span className="font-black text-5xl text-emerald-400">#1</span> Marketing Agency in Pakistan</div>
         <div className="w-full h-[0.5px] bg-gray-700"></div>
         <div className="pt-3 flex items-center gap-5">
@@ -55,16 +55,16 @@ export default function Home() {
       </div>
 
       {/* Services Section */}
-      <section className="pt-20 px-10">
-        <div className="flex items-center gap-10">
+      <section className="md:pt-20 pt-8 px-5 md:px-10">
+        <div className="flex flex-col md:flex-row items-center gap-3 md:gap-10">
           <span className="text-emerald-500 text-xl font-black px-3 py-0.5 rounded-full bg-gray-800">Services</span>
-          <h2 className="font-black text-6xl">What we are offering</h2>
+          <h2 className="font-black text-3xl md:text-6xl">What we are offering</h2>
         </div>
-        <div className="pt-5 flex justify-end">
+        <div className="pt-5 flex justify-center md:justify-end">
           <Button text='VIEW ALL SERVICES' type='button' />
         </div>
-        <div className="grid gap-5 py-25 grid-cols-2">
-          <div className="bg-[#1f1d1d] rounded-4xl h-[370px] p-6">
+        <div className="grid gap-5 md:py-25 py-8 grid-cols-1 md:grid-cols-2">
+          <div className="bg-[#1f1d1d] rounded-4xl h-[430px] md:h-[370px] p-6">
             <div className="bg-[#2a2828] inline-block p-1.5 rounded-xl">
               <FaInstagram className="text-5xl text-emerald-500" />
             </div>
@@ -76,7 +76,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="bg-[#1f1d1d] rounded-4xl h-[370px] p-6">
+          <div className="bg-[#1f1d1d] rounded-4xl h-[430px] md:h-[370px] p-6">
             <div className="bg-[#2a2828] inline-block p-1.5 rounded-xl">
               <FaInstagram className="text-5xl text-emerald-500" />
             </div>
@@ -88,7 +88,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="bg-[#1f1d1d] rounded-4xl h-[370px] p-6">
+          <div className="bg-[#1f1d1d] rounded-4xl h-[430px] md:h-[370px] p-6">
             <div className="bg-[#2a2828] inline-block p-1.5 rounded-xl">
               <FaInstagram className="text-5xl text-emerald-500" />
             </div>
@@ -100,7 +100,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="bg-[#1f1d1d] rounded-4xl h-[370px] p-6">
+          <div className="bg-[#1f1d1d] rounded-4xl h-[430px] md:h-[370px] p-6">
             <div className="bg-[#2a2828] inline-block p-1.5 rounded-xl">
               <FaInstagram className="text-5xl text-emerald-500" />
             </div>
@@ -112,7 +112,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="bg-[#1f1d1d] rounded-4xl h-[370px] p-6">
+          <div className="bg-[#1f1d1d] rounded-4xl h-[430px] md:h-[370px] p-6">
             <div className="bg-[#2a2828] inline-block p-1.5 rounded-xl">
               <FaInstagram className="text-5xl text-emerald-500" />
             </div>
@@ -124,7 +124,7 @@ export default function Home() {
             </div>
           </div>
 
-          <div className="bg-[#1f1d1d] rounded-4xl h-[370px] p-6">
+          <div className="bg-[#1f1d1d] rounded-4xl h-[430px] md:h-[370px] p-6">
             <div className="bg-[#2a2828] inline-block p-1.5 rounded-xl">
               <FaInstagram className="text-5xl text-emerald-500" />
             </div>
@@ -141,41 +141,41 @@ export default function Home() {
 
       {/* Our Expertise Section */}
       <section>
-        <div className="flex px-10 pb-8 items-center gap-10">
+        <div className="flex px-10 pb-8 flex-col md:flex-row items-center gap-3">
           <span className="text-emerald-500 text-xl font-black px-3 py-0.5 rounded-full bg-gray-800">Why us</span>
-          <h2 className="font-black text-5xl">Our Specialities</h2>
+          <h2 className="font-black text-4xl md:text-5xl">Our Specialities</h2>
         </div>
         <div className="flex overflow-hidden relative justify-center ">
           <div className="absolute  inset-0 bg-gradient-to-b   from-black/90 via-black/10 to-transparent" />
           <div className="absolute inset-0 bg-gradient-to-t  from-emerald-500/10 via-emerald-500/90 to-transparent blur-3xl" />
           <div className="absolute z-20 inset-0 bg-gradient-to-t   from-black/90 via-black/10 to-transparent" />
-          <div className="z-10 absolute  pt-10 flex justify-center">
+          <div className="z-10 absolute left-6 md:left-[330px]  pt-10 flex justify-center">
 
 
-            <div className="bg-slate-50/25  h-[180px] gap-2 flex flex-col items-center justify-center w-[180px] rounded-full  ">
-              <div className="text-5xl font-black ">15+</div>
-              <div className="font-bold text-gray-100 text-xl">Years of Experience</div>
+            <div className="bg-slate-50/25 h-[110px] md:h-[180px] gap-2 flex flex-col items-center justify-center w-[110px] md:w-[180px] rounded-full  ">
+              <div className="md:text-5xl text-3xl font-black ">15+</div>
+              <div className="font-bold text-gray-100 text-xs md:text-xl">Years of Experience</div>
             </div>
 
-            <div className="bg-slate-50/25 relative right-5 bottom-5 h-[180px] gap-2 flex flex-col items-center justify-center w-[180px] rounded-full  ">
-              <div className="text-5xl font-black ">200+</div>
-              <div className="font-bold text-gray-100 text-xl">Successfull Projects</div>
+            <div className="bg-slate-50/25 relative right-5 bottom-5 h-[110px] w-[110px] md:h-[180px] gap-2 flex flex-col items-center justify-center md:w-[180px] rounded-full  ">
+              <div className="md:text-5xl text-3xl font-black ">200+</div>
+              <div className="font-bold text-gray-100 text-xs md:text-xl">Successfull Projects</div>
             </div>
 
-            <div className="bg-slate-50/25 relative right-10   h-[180px] gap-2 flex flex-col items-center justify-center w-[180px] rounded-full  ">
-              <div className="text-5xl font-black ">150+</div>
-              <div className="font-bold text-gray-100 text-xl">Happy Clients</div>
+            <div className="bg-slate-50/25 relative right-10  h-[110px] w-[110px] md:h-[180px] gap-2 flex flex-col items-center justify-center md:w-[180px] rounded-full  ">
+              <div className="md:text-5xl text-3xl font-black ">150+</div>
+              <div className="font-bold text-gray-100 text-xs md:text-xl">Happy Clients</div>
             </div>
 
-            <div className="bg-slate-50/25 relative right-15 bottom-5  h-[180px] gap-2 flex flex-col items-center justify-center w-[180px] rounded-full  ">
-              <div className="text-5xl font-black ">200+</div>
-              <div className="font-bold text-gray-100 text-xl">5 Star Reviews</div>
+            <div className="bg-slate-50/25 relative right-15 bottom-5 h-[110px] w-[110px] md:h-[180px] gap-2 flex flex-col items-center justify-center md:w-[180px] rounded-full  ">
+              <div className="md:text-5xl text-3xl font-black ">200+</div>
+              <div className="font-bold text-gray-100 text-xs md:text-xl">5 Star Reviews</div>
             </div>
 
 
           </div>
           <div className="absolute z-20 bottom-0 w-full ">
-            <div className="flex gap-3 px-8  items-center">
+            <div className="flex flex-col md:flex-row gap-3 px-8  items-center">
               <div className="p-5 flex  rounded-xl flex-col gap-1 items-center bg-[#1f1d1d]/40 justify-center ">
                 <div className="p-2 rounded-md mb-4 bg-gray-900/40 border-emerald-500 border">
                   < BsFillRocketFill className="text-emerald-500 text-4xl" />
@@ -187,34 +187,34 @@ export default function Home() {
                 <div className="p-2 rounded-md mb-4 bg-gray-900/40 border-emerald-500 border">
                   < SiHiveBlockchain className="text-emerald-500 text-4xl" />
                 </div>
-                <h3 className="text-3xl font-black">Proven Track Record</h3>
-                <p className="text-lg">We have built a reputation as a trusted and reliable partner in achieving business success.</p>
+                <h3 className="text-3xl font-black">Tailored Solutions</h3>
+                <p className="text-lg">We offer personalized solutions tailored to your specific goals, audience, and industry.</p>
               </div>
                <div className="p-5 flex rounded-xl flex-col gap-1 items-center bg-[#1f1d1d]/40 justify-center ">
                 <div className="p-2 rounded-md mb-4 bg-gray-900/40 border-emerald-500 border">
                   < PiUserFocus className="text-emerald-500 text-4xl" />
                 </div>
-                <h3 className="text-3xl font-black">Proven Track Record</h3>
-                <p className="text-lg">We have built a reputation as a trusted and reliable partner in achieving business success.</p>
+                <h3 className="text-3xl font-black">Client-Centric Focus</h3>
+                <p className="text-lg">Your success is our priority. We prioritize understanding your business goals.</p>
               </div>
             </div>
-            <div className="flex justify-center items-center gap-3">
-              <div className="flex items-center px-3 py-1 rounded-full my-10 gap-1 bg-[#1f1d1d]/60">
+            <div className="flex flex-col my-8 md:my-0 md:flex-row justify-center items-center gap-3">
+              <div className="flex items-center px-3 py-1 rounded-full md:my-10 gap-1 bg-[#1f1d1d]/60">
                 <DiRaphael className="text-emerald-500 text-3xl"/>
                 <div className="text-xl font-bold">Continuous Innovation</div>
 
               </div>
-              <div className="flex items-center px-3 py-1 rounded-full my-10 gap-1 bg-[#1f1d1d]/60">
+              <div className="flex items-center px-3 py-1 rounded-full md:my-10 gap-1 bg-[#1f1d1d]/60">
                 <DiRaphael className="text-emerald-500 text-3xl"/>
                 <div className="text-xl font-bold">Dedicated Support</div>
 
               </div>
-              <div className="flex items-center px-3 py-1 rounded-full my-10 gap-1 bg-[#1f1d1d]/60">
+              <div className="flex items-center px-3 py-1 rounded-full md:my-10 gap-1 bg-[#1f1d1d]/60">
                 <DiRaphael className="text-emerald-500 text-3xl"/>
                 <div className="text-xl font-bold">Positive Client Experiences</div>
 
               </div>
-              <div className="flex items-center px-3 py-1 rounded-full my-10 gap-1 bg-[#1f1d1d]/60">
+              <div className="flex items-center px-3 py-1 rounded-full md:my-10 gap-1 bg-[#1f1d1d]/60">
                 <DiRaphael className="text-emerald-500 text-3xl"/>
                 <div className="text-xl font-bold">Commitment to Excellence</div>
 
@@ -222,7 +222,7 @@ export default function Home() {
             </div>
           </div>
 
-          <Image className="w-[100vw] " quality={100} src={group.src} height={group.height} width={group.width} alt="Hero Image" />
+          <Image className="md:w-[100vw] h-[140vh] object-cover md:h-auto " quality={100} src={group.src} height={group.height} width={group.width} alt="Hero Image" />
         </div>
       </section>
 
